50c02d76a89a892f08ab8bbc07a10fc696c73de4
DearMe <ruanqixiang@gmail.com>
2015-05-28 20:31:08 +0800

    coding DepartmentManage

--- NewsManuscript/src/com/service/DepartmentManage.java
+++ NewsManuscript/src/com/service/DepartmentManage.java
@@ -1,11 +1,14 @@
 package com.service;
 
 
+import com.exceptions.departmentManage.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.dao.ComDepartmentDAO;
 import com.domain.ComDepartment;
+
+import java.util.List;
 
 public class DepartmentManage {
 	/* new department
@@ -13,44 +16,136 @@ public class DepartmentManage {
 	 */
 	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 	ComDepartmentDAO comDepartmentDAO = (ComDepartmentDAO)applicationContext.getBean("ComDepartmentDAO");
+
+	private boolean checkNameIsOnly(String departmentName){
+		List<ComDepartment> comDepartmentList = comDepartmentDAO.findByDepartmentName(departmentName);
+		return (comDepartmentList == null || comDepartmentList.size() <= 0);
+	}
+
+	private boolean checkNameSizeIsLegal(String departmentName){
+		return departmentName.length() > 0 && departmentName.length() <= 32;
+	}
+
+	private boolean checkHasSubDepartment(String departmentId){
+		List<ComDepartment> comDepartmentList = comDepartmentDAO.findByParentDepartment(departmentId);
+		return (comDepartmentList != null || comDepartmentList.size() > 0);
+	}
+
+	private boolean checkDepartmentIsExistent(String departmentId){
+		ComDepartment comDepartment = comDepartmentDAO.findById(departmentId);
+		return (comDepartment != null && comDepartment.getDepartmentId() != null);
+	}
+
 	public void addDepartment(ComDepartment department){
 
 		try{
 			/*
 			 * check the object department  
 			 */
-			if(department == null) throw new Exception("department is null");
+			if(department == null)
+				throw new NullPointerException("department is null");
 			
 			/*
 			 * check department's name
 			 */
-			if(department.getDepartmentName() == null){ 
-				throw new Exception("department's name is null");
-			}else {
-				if(department.getDepartmentName().length() <= 0 || department.getDepartmentName().length() > 32)
-					throw new Exception("size of depatment's name is not in (0, 32]");
-			}
-			
+			if(department.getDepartmentName() == null)
+				throw new NullPointerException("department's name is null");
+			else if(!checkNameSizeIsLegal(department.getDepartmentName()))
+				throw new DepartmentNameException("size of department's name is not in (0, 32]");
+			else if(!checkNameIsOnly(department.getDepartmentName()))
+				throw new DepartmentNameException("name of department is existent");
+
 			/*
-			 * check department's parentDepartmen
+			 * check department's parentDepartment
 			 */
 			if(department.getParentDepartment() != null){
-				ComDepartment comDepartmentParent = comDepartmentDAO.findById(department.getParentDepartment());
-				if(comDepartmentParent == null){
-					throw new Exception("parent department is not existence");
-				}
+				if(!checkDepartmentIsExistent(department.getParentDepartment()))
+					throw new NullPointerException("parent-department is not existence");
 			}
+
+
 			department.setStatus(0);
 			
 			comDepartmentDAO.save(department);
 			
-		}catch(Exception e){
-			System.out.println(e);
+		}catch(DepartmentNameException e){
+			System.out.println(e.toString());
 		}
 	}
 	
 	public void delDepartment(ComDepartment department){
-		
-			
+		try{
+			/*
+			 *check department
+			 */
+			if(department == null || department.getDepartmentId() == null)
+				throw new NullPointerException("department is null");
+
+			/*
+			 *check department is existent or not
+			 */
+			ComDepartment comDepartment = comDepartmentDAO.findById(department.getDepartmentId());
+			if(comDepartment == null || comDepartment.getDepartmentId() == null)
+				throw new DepartmentDelNotExistentException("department is not existent");
+
+			/*
+			 *check sub-department
+			 */
+			if(checkHasSubDepartment(department.getDepartmentId()))
+				throw new DepartmentDelSubdepartmentExistentException("department has some sub-departments");
+
+			department.setStatus(1);
+			comDepartmentDAO.attachDirty(department);
+
+		}catch(DepartmentDelNotExistentException e){
+			System.out.println(e.toString());
+		}catch(DepartmentDelSubdepartmentExistentException e){
+			System.out.println(e.toString());
+		}
+	}
+
+	public void updDepartment(ComDepartment department){
+		try{
+			/*
+			 *check department
+			 */
+			if(department == null || department.getDepartmentId() == null)
+				throw new NullPointerException("department is null");
+
+			/*
+			 *check department is existent or not
+			 */
+			if(!checkDepartmentIsExistent(department.getDepartmentId()))
+				throw new DepartmentUpdNotExistentException("department is not existent");
+
+			/*
+			 *check department's name
+			 */
+			if(department.getDepartmentName() == null)
+				throw new NullPointerException("department's name is null");
+			else if(!checkNameSizeIsLegal(department.getDepartmentName()))
+				throw new DepartmentNameSizeException("size of department's name is not in (0, 32]");
+			else if(!checkNameIsOnly(department.getDepartmentName()))
+				throw new DepartmentNameException("name of department is existent");
+
+			/*
+			 * check department's parentDepartment
+			 */
+			if(department.getParentDepartment() != null){
+				if(!checkDepartmentIsExistent(department.getParentDepartment()))
+					throw new NullPointerException("parent-department is not existence");
+			}
+
+			department.setStatus(0);
+
+			comDepartmentDAO.attachDirty(department);
+
+		}catch(DepartmentUpdNotExistentException e){
+			System.out.println(e.toString());
+		}catch(DepartmentNameException e){
+			System.out.println(e.toString());
+		}catch(DepartmentNameSizeException e){
+			System.out.println(e.toString());
+		}
 	}
 }
--- NewsManuscript/src/com/test/TestMain.java
+++ NewsManuscript/src/com/test/TestMain.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import com.service.DepartmentManage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,13 +12,14 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext aContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		TestService tsService = (TestService)aContext.getBean("testService");
-		System.out.println(tsService.getName());
-//		ComDepartment comDepartment = new ComDepartment();
-//		comDepartment.setDepartmentName("abc");
-//		comDepartment.setStatus(0);
-//		ComDepartmentDAO comDepartmentDAO = (ComDepartmentDAO)aContext.getBean("ComDepartmentDAO");
-//		comDepartmentDAO.save(comDepartment);
+
+		ComDepartment comDepartment = new ComDepartment();
+		comDepartment.setDepartmentId("2ce4e7854d99c1d0014d99c1d5d50000");
+		comDepartment.setDepartmentName("abc");
+		comDepartment.setStatus(0);
+
+		DepartmentManage departmentManage = new DepartmentManage();
+		departmentManage.delDepartment(comDepartment);
 	}
 
 }
