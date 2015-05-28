5c310e9455b2556281f2ffed21ecaae9ecf0c025
DearMe <453386027@qq.com>
2015-05-28 08:57:42 +0800

    complete com.service.DepartmentManage.addDepartment

--- NewsManuscript/src/com/service/DepartmentManage.java
+++ NewsManuscript/src/com/service/DepartmentManage.java
@@ -11,13 +11,46 @@ public class DepartmentManage {
 	/* new department
 	 * 
 	 */
+	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+	ComDepartmentDAO comDepartmentDAO = (ComDepartmentDAO)applicationContext.getBean("ComDepartmentDAO");
 	public void addDepartment(ComDepartment department){
+
 		try{
-			if(department == null) throw new Exception("Department's name's length must be less than 32 chars");
+			/*
+			 * check the object department  
+			 */
+			if(department == null) throw new Exception("department is null");
+			
+			/*
+			 * check department's name
+			 */
+			if(department.getDepartmentName() == null){ 
+				throw new Exception("department's name is null");
+			}else {
+				if(department.getDepartmentName().length() <= 0 || department.getDepartmentName().length() > 32)
+					throw new Exception("size of depatment's name is not in (0, 32]");
+			}
+			
+			/*
+			 * check department's parentDepartmen
+			 */
+			if(department.getParentDepartment() != null){
+				ComDepartment comDepartmentParent = comDepartmentDAO.findById(department.getParentDepartment());
+				if(comDepartmentParent == null){
+					throw new Exception("parent department is not existence");
+				}
+			}
 			department.setStatus(0);
-			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+			
+			comDepartmentDAO.save(department);
+			
 		}catch(Exception e){
 			System.out.println(e);
 		}
+	}
+	
+	public void delDepartment(ComDepartment department){
+		
+			
 	}
 }
